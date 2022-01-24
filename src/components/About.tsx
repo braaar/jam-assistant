9928d8076051488f07893029844cf057d53b4581
Pankaj Sharma <pankaj.sharma@appventurez.com>
2022-01-25 00:48:33 +0530

    Add About Component

---
+++ src/components/About.tsx
@@ -0,0 +1,14 @@
+import { Box, Button, Flex, Heading, Paragraph } from "theme-ui";
+
+export const About = () => {
+  return (
+    <Box>
+      <Heading>About</Heading>
+      <Paragraph variant="block">
+        Hi there 👋, Welcome in Jam Assistant. A free open-source web app that
+        can help you come up with things to play when jamming live music. Can
+        also be used for solo practice.
+      </Paragraph>
+    </Box>
+  );
+};
