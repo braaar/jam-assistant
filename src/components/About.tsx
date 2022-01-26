91399dd5746f9762b9c7944959ff87415f51f926
Pankaj Sharma <pankajsharmavats88@gmail.com>
2022-01-26 16:26:16 +0530

    remove paragraph variant block

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -4,7 +4,7 @@ export const About = () => {
   return (
     <Box>
       <Heading>About</Heading>
-      <Paragraph variant="block">
+      <Paragraph>
         Hi there 👋, Welcome in Jam Assistant. A free open-source web app that
         can help you come up with things to play when jamming live music. Can
         also be used for solo practice.
