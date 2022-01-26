75d4b1bc366f7e751710da84b134f2c2a7fdec76
Pankaj Sharma <pankajsharmavats88@gmail.com>
2022-01-26 18:13:19 +0530

    Add About Component (#27)
    * Add About Component
    
    * remove paragraph variant block
    
    * update about me text
    
    * rename file
    
    Co-authored-by: Pankaj Sharma <pankaj.sharma@appventurez.com>
    Co-authored-by: Brage <brage.aarset@gmail.com>

---
+++ src/components/about.tsx
@@ -0,0 +1,14 @@
+import { Box, Button, Flex, Heading, Paragraph } from "theme-ui";
+
+export const About = () => {
+  return (
+    <Box>
+      <Heading>About</Heading>
+      <Paragraph>
+        Welcome to Jam Assistant, a free open-source web app that
+        can help you come up with things to play when jamming live music. Can
+        also be used for solo practice.
+      </Paragraph>
+    </Box>
+  );
+};
